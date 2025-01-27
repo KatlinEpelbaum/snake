897b18487e60ce14b31ee9cd6cc237525536a1b6
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-01-27 11:29:54 +0200

    Movements

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const speed = 200;
 gameBoardDiv.style.gridTemplateColumns = `repeat(${width}, 8px)`;
 gameBoardDiv.style.gridTemplateRows = `repeat(${height}, 8px)`;
 
-let snake = [`${Math.floor(width / 2)}_${Math.floor(height / 2)}`];
+let snake = [`${Math.floor(height / 2)}_${Math.floor(width / 2)}`];
 let direction = 'up';
 
 const intervalId = setInterval(run, speed);
@@ -17,6 +17,28 @@ function run() {
     updateSnake();
     drawGameBoard();
 }
+
+document.addEventListener('keydown', e => {
+
+    switch (e.key.toLocaleLowerCase()) {
+        case 'arrowup':
+        case 'w':
+            direction = 'up'
+            break;
+        case 'arrowdown':
+        case 's':
+            direction = 'down'
+            break;
+        case 'arrowright':
+        case 'd':
+            direction = 'right'
+            break;
+        case 'arrowleft':
+        case 'a':
+            direction = 'left'
+            break;
+    }
+})
 
 function drawGameBoard() {
     gameBoardDiv.innerHTML = '';
@@ -39,16 +61,32 @@ function updateSnake() {
 
     switch (direction) {
         case 'up':
-            y--;
+            if ( y == 0){
+                y = height -1
+            }else{
+                y--;
+            }
             break;
         case 'down':
-            y++;
+            if ( y == height -1){
+                y = 0;
+            } else {
+                y++;
+            }
             break;
         case 'left':
+            if( x == 0){
+                x = width -1
+            } else{
             x--;
+            }
             break;
         case 'right':
+            if( x == width -1){
+                x = 0
+            } else {
             x++;
+            }
             break;
     }
 
