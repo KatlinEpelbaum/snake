3b0e15adb97e69a8009c2e3ff7c743250cd848a4
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-01-27 10:53:50 +0200

    snake dot

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ const width = 40;
 gameBoardDiv.style.gridTemplateColumns = `repeat(${width}, 8px)`;
 gameBoardDiv.style.gridTemplateRows = `repeat(${height}, 8px)`;
 
-for (let y = 0; y < height; y++) { 
-    for (let x = 0; x < width; x++) {
-        const cellDiv = document.createElement('div');
-        gameBoardDiv.appendChild(cellDiv);
+let snake = [Math.floor(width / 2)+ '_' + Math.floor(height / 2)];
+
+drawGameBoard();
+
+function drawGameBoard(){
+
+    for (let y = 0; y < width; y++) { 
+        for (let x = 0; x < height; x++) {
+            let cellDiv = document.createElement('div');
+
+            if( snake.includes(`${y}_${x}`)){
+                cellDiv.style.backgroundColor = 'red';
+            }
+
+            gameBoardDiv.appendChild(cellDiv);
+        }
     }
 }
+
